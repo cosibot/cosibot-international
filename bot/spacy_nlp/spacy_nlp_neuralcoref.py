2db953745f3e42e07e13ac7c0da94001d35bbbbf
martasls <martasls95@gmail.com>
2020-06-08 14:51:03 +0100

    Initial neuralcoref solution

--- bot/spacy_nlp/spacy_nlp_neuralcoref.py
+++ bot/spacy_nlp/spacy_nlp_neuralcoref.py
@@ -9,6 +9,7 @@ from rasa.nlu.config import RasaNLUModelConfig, override_defaults
 from rasa.nlu.training_data import Message, TrainingData
 from rasa.nlu.model import InvalidModelError
 from rasa.core.slots import TextSlot
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -43,41 +44,55 @@ class SpacyNLPNeuralCoref(SpacyNLP):
                 component_config: Dict[Text, Any] = None,
                 nlp: "Language" = None,
                 max_history: int = 2) -> None:
-
+        print("init Spacy")
+        if not exists('./spacy_nlp/history'):
+            os.makedirs('./spacy_nlp/history')
+        self.session_token = str(datetime.now().strftime("%Y%m%d_%H%M%S.%f"))
         self.max_history = max_history
         super().__init__(component_config, nlp)
 
+    @staticmethod
+    def load_model(spacy_model_name: Text) -> "Language":
+        """Try loading the model, catching the OSError if missing."""
+        import spacy
+
+        try:
+            return spacy.load(spacy_model_name)
+        except OSError:
+            raise InvalidModelError(
+                "Model '{}' is not a linked spaCy model.  "
+                "Please download and/or link a spaCy model, "
+                "e.g. by running:\npython -m spacy download "
+                "en_core_web_md\npython -m spacy link "
+                "en_core_web_md en".format(spacy_model_name)
+            )
 
     def doc_for_text(self, text: Text) -> "Doc":
-        text = "why should i wear a mask? where can i buy it?"
-        nlp = spacy.load("en_neuralcoref")
-        print(nlp==self.nlp)
-        doc = nlp(text)
+        # text = "why should i wear a mask? where can i buy it?"
+        # nlp = spacy.load("en_neuralcoref")
+        # print(nlp==self.nlp)
+        # doc = nlp(text)
 
-        history_dir = './spacy_nlp/history'
-        if not exists(history_dir):
+        history_file_path = join('./spacy_nlp/history', 'neuralcoref_hist_'+self.session_token+'.txt')
+        if not exists(history_file_path):
             # if there's no file with previous user messages
             # generate doc with current message
             # this doc is generated to have access to doc._.coref_resolved
             preprocessed_text = self.preprocess_text(text)
-            print(preprocessed_text=="why should i wear a mask? where can i buy it?")
             doc = self.nlp(preprocessed_text)
             result = doc._.coref_resolved
             # we can now save this doc._.coref_resolved in a file
-            os.makedirs(history_dir)
-            with open(join(history_dir, 'history.txt'), 'a', encoding='utf-8') as file:
+            with open(history_file_path, 'a', encoding='utf-8') as file:
                 file.write(result)
-        elif exists(history_dir):
-            with open(join(history_dir, 'history.txt'), 'r', encoding='utf-8') as file:
+        elif exists(history_file_path):
+            with open(history_file_path, 'r', encoding='utf-8') as file:
                 lines = [line.strip() for line in file.readlines()][-self.max_history:]
             previous_sentences = (' ').join(lines)
             to_evaluate = previous_sentences + " " + text
             preprocessed_text = self.preprocess_text(to_evaluate)
             doc = self.nlp(preprocessed_text)
             result = doc._.coref_resolved[-((len(doc._.coref_resolved)-len(previous_sentences))-1):]
-            print("history {}".format(result))
-            print(to_evaluate)
-            with open(join(history_dir, 'history.txt'), 'a', encoding='utf-8') as file:
+            with open(history_file_path, 'a', encoding='utf-8') as file:
                 file.write("\n")
                 file.write(result)
         # now we generate a new doc based on doc._.coref_resolved
