4c1837dc5096388166159f57dac196114e033717
FSamina <fabiosamina@gmail.com>
2020-08-06 17:07:23 +0100

    lookupTables2synonym.py

---
+++ scripts/python/lookupTables2synonym.py
@@ -0,0 +1,28 @@
+import os
+
+entries = os.listdir('lookup_tables/')
+with open('synonym.txt', 'w') as s:
+    for entry in entries:
+        #print(entry[:-4])
+        s.write("## synonym:"+entry[:-4])
+        with open('lookup_tables/'+ entry, 'r') as f:
+            data = f.read().split("\n")
+            for line in data:
+                #print(len(line))
+                if(len(line)>0):
+                    s.write("\n- "+ line)
+                else:
+                    s.write("\n\n")
+            
+
+
+"""
+## synonym:EG
+- República Árabe do Egipto
+- República Árabe do Egito
+- Egipto
+- Egito
+- Arab Republic of Egypt
+- Egypt
+
+"""
